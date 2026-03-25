189a14bf8c379a879d39367a6b4f58f4985cd96a
Tanishq Jagwani <tanishqjagwani0@gmail.com>
2026-03-26 03:58:38 +0530

    update configg

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 RESEND_API_KEY = os.getenv("RESEND_API_KEY", "")
 NEWS_API_KEY = os.getenv("NEWS_API_KEY", "")
 EMAIL_TO = os.getenv("EMAIL_TO", "")
-EMAIL_FROM = os.getenv("EMAIL_FROM", "Stock Genie <stockgenie@resend.dev>")
+EMAIL_FROM = os.getenv("EMAIL_FROM", "Stock Genie <onboarding@resend.dev>")
 
 YFINANCE_DELAY = 0.5  # seconds between requests
 NEWS_DAYS_BACK = 1
